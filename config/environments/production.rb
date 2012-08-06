34fa2f9c2dbd9b7e00373923ff8a24bb2020ff09
Tyrel Richey <tyrelrichey@gmail.com>
2012-08-06 17:18:59 -0600

    Changed config for production to temp have precompiling enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ CannaPages::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += ['cadets.css']
   # Generate digests for assets URLs
   config.assets.digest = true
