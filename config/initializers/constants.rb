374a1c2773ae9d1dec415a5ec82aa98e9ed198fb
tyrel86 <tyrelrichey@gmail.com>
2012-11-06 23:23:06 -0700

    Small astetic fixes

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,8 +1,8 @@
 LEGAL_STATE_ARRAY = [ 'Alaska', 'Arizona', 'California', 'Colorado', 
-                                          'Connecticut', 'DC', 'Delaware', 'Hawaii, Maine', 'Michigan', 
-                                          'Montana', 'Initiative', 'Nevada', 'New Jersey', 'New Mexico', 
-                                          'Oregon', 'Rhode Island' ]
-                                          
-YESNO = ['yes','no']
+                      'Connecticut', 'DC', 'Delaware', 'Hawaii, Maine', 'Michigan', 
+                      'Montana', 'Initiative', 'Nevada', 'New Jersey', 'New Mexico', 
+                      'Massachusetts', 'Oregon', 'Rhode Island' ] 
+                                                                   
+YESNO = ['yes','no'] 
 
-BUSINESS_TYPE_ARRAY = [ 'Dispensary', 'Grow Store', 'Head Shop', 'Kind Doctor', 'Kind Land Lord' ]
+BUSINESS_TYPE_ARRAY = [ 'Dispensary', 'Grow Store', 'Head Shop', 'Kind Doctor', 'Kind Land Lord' ] 
